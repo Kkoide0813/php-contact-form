a0f627e6d531dc2d683f4851f2a79e8b32756d2c
KoideKeiji <qwejks0201@gmail.com>
2026-03-13 00:48:09 +0900

    フォーム基本形作成

--- form/input.php
+++ form/input.php
@@ -1,29 +1,13 @@
 <?php
 
-// aaaa
-
-// ログ
-if (!empty($_POST)) {
+if(!empty($_POST)){
     echo '<pre>';
     var_dump($_POST);
     echo '</pre>';
 }
 
-// 入力画面
-$pageFlag = 0;
-
-// 確認画面へ
-if (!empty($_POST['btn_confirm'])) {
-    $pageFlag = 1;
-}
-
-// 完了画面へ
-if (!empty($_POST['btn_submit'])) {
-    $pageFlag = 2;
-}
-
-
 ?>
+
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -31,45 +15,20 @@ if (!empty($_POST['btn_submit'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>お問い合わせフォーム</title>
+    <title>お問い合わせ</title>
 </head>
 
 <body>
-    <!-- 入力画面 -->
-    <?php if ($pageFlag === 0) : ?>
-        <form method="POST" action="input.php">
-            氏名
-            <input type="text" name="your_name" value="<?php if (!empty($_POST['your_name'])) {echo $_POST['your_name'];} ?>">
-            <br>
-            メールアドレス
-            <input type="email" , name="email" value="<?php if (!empty($_POST['email'])) {echo $_POST['email'];} ?>">
-            <br>
-            <input type="submit" name="btn_confirm" , value="確認する">
-        </form>
-    <?php endif; ?>
-
-    <!-- 確認画面 -->
-    <?php if ($pageFlag === 1) : ?>
-        <form method="POST" action="input.php">
-            氏名
-            <?php echo $_POST['your_name']; ?>
-            <br>
-            メールアドレス
-            <?php echo $_POST['email']; ?>
-            <br>
-            <input type="submit" name="back" , value="戻る">
-            <input type="submit" name="btn_submit" , value="送信する">
-            <input type="hidden" name="your_name" value="<?php echo $_POST['your_name']; ?>">
-            <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
-
-        </form>
-    <?php endif; ?>
-
-    <!-- 完了画面 -->
-    <?php if ($pageFlag === 2) : ?>
-        送信が完了しました。
-    <?php endif; ?>
-
+    <form method="POST" action="input.php">
+        氏名
+        <input type="text" name="your_name">
+        <br>
+        <input type="checkbox" name="sports" value="サッカー">サッカー
+        <input type="checkbox" name="sports" value="野球">野球
+        <input type="checkbox" name="sports" value="バスケ">バスケ
+        <br>
+        <input type="submit" value="送信">
+    </form>
 </body>
 
 </html>

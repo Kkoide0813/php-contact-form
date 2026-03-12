4bb48ce62731a8f69125cf78175b9ca288f374e0
KoideKeiji <qwejks0201@gmail.com>
2026-03-13 00:16:21 +0900

    戻るボタン追加

--- form/input.php
+++ form/input.php
@@ -39,14 +39,10 @@ if (!empty($_POST['btn_submit'])) {
     <?php if ($pageFlag === 0) : ?>
         <form method="POST" action="input.php">
             氏名
-            <input type="text" name="your_name" value="<?php if (!empty($_POST['your_name'])) {
-                                                            echo $_POST['your_name'];
-                                                        } ?>">
+            <input type="text" name="your_name" value="<?php if (!empty($_POST['your_name'])) {echo $_POST['your_name'];} ?>">
             <br>
             メールアドレス
-            <input type="email" , name="email" value="<?php if (!empty($_POST['email'])) {
-                                                            echo $_POST['email'];
-                                                        } ?>">
+            <input type="email" , name="email" value="<?php if (!empty($_POST['email'])) {echo $_POST['email'];} ?>">
             <br>
             <input type="submit" name="btn_confirm" , value="確認する">
         </form>

dd6824730044887e8ed12fe935a3f17fe957859f
KoideKeiji <qwejks0201@gmail.com>
2026-03-14 16:37:35 +0900

    CSRF対策トークン機能追加

--- form/input.php
+++ form/input.php
@@ -1,10 +1,13 @@
 <?php
 
-// if (!empty($_POST)) {
-//     echo '<pre>';
-//     var_dump($_POST);
-//     echo '</pre>';
-// }
+session_start();
+header('X-FRAME-OPTIONS:DENY');
+
+if (!empty($_SESSION)) {
+    echo '<pre>';
+    var_dump($_SESSION);
+    echo '</pre>';
+}
 
 // XSS対策
 function h($str)
@@ -36,8 +39,18 @@ if (!empty($_POST['btn_submit'])) {
 </head>
 
 <body>
-    <!-- 入力画面 -->
+    <!-- 入力画面 $pageFlag === 0-->
     <?php if ($pageFlag === 0) : ?>
+        <?php
+        // csrfTokenが設定されていなければトークン作成
+        if (!isset($_SESSION['csrfToken'])) {
+            // random_bytes(24~32) ランダムな暗号作成し、bin2hexでバイナリからトークン化する
+            $csrfToken = bin2hex(random_bytes(32));
+            $_SESSION['csrfToken'] = $csrfToken;
+        }
+        $token = $_SESSION['csrfToken'];
+        ?>
+
         <form method="POST" action="input.php">
             氏名
             <input type="text" name="your_name"
@@ -52,29 +65,41 @@ if (!empty($_POST['btn_submit'])) {
                         } ?>">
             <br>
             <input type="submit" name="btn_confirm" value="確認する">
+            <input type="hidden" name="csrf" value="<?php echo $token; ?>">
         </form>
     <?php endif; ?>
 
-    <!-- 確認画面 -->
+    <!-- 確認画面 $pageFlag === 1-->
     <?php if ($pageFlag === 1) : ?>
-        <form method="POST" action="input.php">
-            氏名
-            <?php echo h($_POST['your_name']) ?>
-            <br>
-            メールアドレス
-            <?php echo h($_POST['email']) ?>
-            <br>
-            <input type="submit" name="btn_submit" value="送信する">
-            <input type="submit" name="back" value="戻る">
-            <!-- 画面が切り替わっても入力内容のデータを保持する必要がある.表に出さないのでhidden -->
-            <input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']); ?>">
-            <input type="hidden" name="email" value="<?php echo h($_POST['email']); ?>">
-        </form>
+        <!-- トークンが一致しているか判定 -->
+        <?php if ($_POST['csrf'] === $_SESSION['csrfToken']) : ?>
+            <form method="POST" action="input.php">
+                氏名
+                <?php echo h($_POST['your_name']) ?>
+                <br>
+                メールアドレス
+                <?php echo h($_POST['email']) ?>
+                <br>
+                <input type="submit" name="btn_submit" value="送信する">
+                <input type="submit" name="back" value="戻る">
+                <!-- 画面が切り替わっても入力内容のデータを保持する必要がある.表に出さないのでhidden -->
+                <input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']); ?>">
+                <input type="hidden" name="email" value="<?php echo h($_POST['email']); ?>">
+                <input type="hidden" name="csrf" value="<?php echo h($_POST['csrf']); ?>">
+            </form>
+        <?php endif; ?>
+
     <?php endif; ?>
 
-    <!-- 完了画面 -->
+    <!-- 完了画面 $pageFlag === 2-->
     <?php if ($pageFlag === 2) : ?>
-        送信が完了しました。
+        <!-- 完了画面でも合言葉があっていたかを確認する -->
+        <?php if ($_POST['csrf'] === $_SESSION['csrfToken']) : ?>
+            送信が完了しました。
+
+            <!-- 合言葉がずっと残らないようにセッションを削除する。 -->
+            <?php unset($_SESSION['csrfToken']); ?>
+        <?php endif; ?>
         <form>
             <input type="submit" name="back" value="戻る">
         </form>
